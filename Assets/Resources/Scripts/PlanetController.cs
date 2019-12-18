8f76bdf6ae3f7ef97228bef6364943e0aef8a93b
skeleton625 <chaos0824@naver.com>
2019-12-18 23:30:52 +0900

    Add Shrinking Planet Controller Script

---
+++ Assets/Resources/Scripts/PlanetController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetController : MonoBehaviour
+{
+    [SerializeField]
+    private float ShrinkForce;
+    [SerializeField]
+    private float MinShrink;
+
+    private Vector3 ShrinkVector;
+    private WaitForSeconds ShrinkTimer = new WaitForSeconds(1f);
+
+    private void Start()
+    {
+        ShrinkVector = new Vector3(1, 1, 1) * ShrinkForce;
+        StartCoroutine(ShrinkPlanetCoroutine());
+    }
+
+    private IEnumerator ShrinkPlanetCoroutine()
+    {
+        while(transform.localScale.x > MinShrink)
+        {
+            transform.localScale -= ShrinkVector;
+            yield return null;
+        }       
+    }
+}
