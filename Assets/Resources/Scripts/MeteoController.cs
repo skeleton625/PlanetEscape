4c4033858d2e9f0ed373629042e565b3848b5149
skeleton625 <chaos0824@naver.com>
2019-12-20 18:39:57 +0900

    Add MeteoController script

---
+++ Assets/Resources/Scripts/MeteoController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteoController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject MeteoEffect;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject _effect = Instantiate(MeteoEffect, collision.contacts[0].point, Quaternion.identity);
+        Destroy(_effect, 1.5f);
+        Destroy(gameObject);
+    }
+}
