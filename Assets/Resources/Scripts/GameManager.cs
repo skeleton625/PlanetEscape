c0a5273d45ed653f47d3185c72ffb7f24a182fca
skeleton625 <chaos0824@naver.com>
2019-12-23 16:08:47 +0900

    Add Game Controller script

---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private InGameUI GameUI;
+    private GameObject Planet;
+    private GameObject PlayerCar;
+    private PlayerController PlayerInstance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameUI = GetComponent<InGameUI>();
+        PlayerCar = GameObject.Find("PlayerCar");
+        PlayerInstance = PlayerCar.GetComponent<PlayerController>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(!PlayerInstance.PlayerDead)
+            GameUI.PrintPlanetRadius();
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        if(PlayerInstance.PlayerDead)
+        {
+        }
+    }
+}
