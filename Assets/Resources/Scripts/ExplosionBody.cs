cf695f0f023e5cde5e8e3a51ff4e360a547932df
skeleton625 <chaos0824@naver.com>
2019-12-23 16:07:20 +0900

    Add Explosion Effect's effect script for limit of explosion collider time

---
+++ Assets/Resources/Scripts/ExplosionBody.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionBody : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(ExplosionBodyCoroutine());
+    }
+
+    private IEnumerator ExplosionBodyCoroutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+        gameObject.SetActive(false);
+    }
+}
