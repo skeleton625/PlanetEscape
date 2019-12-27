f28b3940fe73c3e6d8c39931e35773e7a1d75ca7
skeleton625 <chaos0824@naver.com>
2019-12-27 14:07:50 +0900

    Modify GameManager for Administrating PlanetController to GameManager

--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -7,27 +7,34 @@ public class GameManager : MonoBehaviour
     private InGameUI GameUI;
     private GameObject Planet;
     private GameObject PlayerCar;
+    private IEnumerator MeteoCoroutine;
     private PlayerController PlayerInstance;
+    private PlanetController PlanetInstance;
 
     // Start is called before the first frame update
     void Start()
     {
         GameUI = GetComponent<InGameUI>();
+        Planet = GameObject.Find("Planet");
         PlayerCar = GameObject.Find("PlayerCar");
+        PlanetInstance = Planet.GetComponent<PlanetController>();
         PlayerInstance = PlayerCar.GetComponent<PlayerController>();
-    }
-
-    private void FixedUpdate()
-    {
-        if(!PlayerInstance.PlayerDead)
-            GameUI.PrintPlanetRadius();
+        MeteoCoroutine = PlanetInstance.StartMeteoCoroutine();
+        StartCoroutine(MeteoCoroutine);
     }
 
     // Update is called once per frame
-    private void LateUpdate()
+    private void FixedUpdate()
     {
-        if(PlayerInstance.PlayerDead)
+        if (!PlayerInstance.PlayerDead)
         {
+            GameUI.PrintPlanetRadius();
+            PlanetInstance.ShrinkingPlanet();
         }
+        else
+        {
+            GameUI.ActivateGameOverUI();
+            StopCoroutine(MeteoCoroutine);
+        }   
     }
 }
