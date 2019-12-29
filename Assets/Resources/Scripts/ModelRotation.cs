f226aa77ea53d92cc30dd46cf688d295f6671388
WaniFi <chaos0824@naver.com>
2019-12-29 18:59:47 +0900

    Add Model rotation script for Using in menu

---
+++ Assets/Resources/Scripts/ModelRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelRotation : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 RotateVector;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(RotateVector);
+    }
+}
