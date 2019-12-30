ee02a931f44f436aff503268a3c49315647ae997
skeleton625 <chaos0824@naver.com>
2019-12-30 13:58:17 +0900

    Add Menu scene ui script

---
+++ Assets/Resources/Scripts/MenuGameUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MenuGameUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject StartGameObjects;
+    [SerializeField]
+    private GameObject StartUI;
+    [SerializeField]
+    private GameObject ChooseGameObjects;
+    [SerializeField]
+    private GameObject ChooseUI;
+
+    public void ActivateGameStartObjects(bool activate)
+    {
+        if (activate)
+        {
+            StartGameObjects.SetActive(false);
+            StartUI.SetActive(false);
+            ChooseGameObjects.SetActive(true);
+            ChooseUI.SetActive(true);
+        }
+        else
+        {
+            StartGameObjects.SetActive(true);
+            StartUI.SetActive(true);
+            ChooseGameObjects.SetActive(false);
+            ChooseUI.SetActive(false);
+        }
+    }
+}
