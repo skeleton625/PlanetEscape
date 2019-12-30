24fde2ed1eeb80daa655ceaa98dc16b0f669ee65
skeleton625 <chaos0824@naver.com>
2019-12-30 13:57:53 +0900

    Change GameObjectArray script to ObjectManager script and separate ui script functions

---
+++ Assets/Resources/Scripts/ObjectManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] CarObjects;
+    public static int PreCarNumber;
+
+    private void Awake()
+    {
+        ObjectManager.PreCarNumber = 0;
+    }
+
+    public void ActivateCarObject(bool _isRight)
+    {
+        CarObjects[PreCarNumber].SetActive(false);
+        CarObjects[PreCarNumber].transform.rotation = Quaternion.identity;
+
+        if (_isRight)
+        {
+            ++PreCarNumber;
+            if (PreCarNumber >= CarObjects.Length)
+                PreCarNumber = 0;
+        }
+        else
+        {
+            --PreCarNumber;
+            if (PreCarNumber < 0)
+                PreCarNumber = CarObjects.Length - 1;
+        }
+
+        CarObjects[PreCarNumber].SetActive(true);
+    }
+}
